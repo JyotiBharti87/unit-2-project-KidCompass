1c8893b55915dd7ae8d6d9555f666bc11a43dd03
jyotibharti87 <joybharti87@gmail.com>
2026-09-07 18:24:33 -0500

    commit-8

--- kidcompass-backend/src/main/java/com/kidcompass/controllers/EventController.java
+++ kidcompass-backend/src/main/java/com/kidcompass/controllers/EventController.java
@@ -1,4 +1,32 @@
 package com.kidcompass.controllers;
 
+import com.kidcompass.models.Event;
+import com.kidcompass.models.Parent;
+import com.kidcompass.repositories.EventRepository;
+import com.kidcompass.repositories.ParentRepository;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/events")
 public class EventController {
+
+    private final EventRepository eventRepository;
+    private final ParentRepository parentRepository;
+
+    //constructor injection for the EventRepository and ParentRepository
+    public EventController(EventRepository eventRepository, ParentRepository parentRepository) {
+        this.eventRepository = eventRepository;
+        this.parentRepository = parentRepository;
+    }
+
+    //Returns a list of all events in the database.
+    @GetMapping
+    public List<Event>getAllEvents() {
+        return eventRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public Event getEventById(@PathVariable Long id) {
+        return eventRepository.findById(id).orElse(null);
+
 }
