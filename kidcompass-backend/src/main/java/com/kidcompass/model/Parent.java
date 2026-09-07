e62caa776c842831c036c13d281ce9987cd6ed11
jyotibharti87 <joybharti87@gmail.com>
2026-09-07 15:06:27 -0500

    commit-2

--- kidcompass-backend/src/main/java/com/kidcompass/model/Parent.java
+++ kidcompass-backend/src/main/java/com/kidcompass/model/Parent.java
@@ -1,4 +1,28 @@
 package com.kidcompass.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Parent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String city;
+    private String state;
+    private String country;
+    private String kidName;
+    private Integer kidAge;
+    private String kidGender;
+    private String kidBio;
+
+    public Parent() {}
+
+
 }
