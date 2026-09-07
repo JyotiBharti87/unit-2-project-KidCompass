e6182cf32e363c1c314fdeccccf7bf44297739bf
jyotibharti87 <joybharti87@gmail.com>
2026-09-07 14:13:55 -0500

    InitialSetUp

---
+++ kidcompass-backend/src/main/java/com/kidcompass/KidcompassBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kidcompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidcompassBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidcompassBackendApplication.class, args);
+	}
+
+}
---
+++ kidcompass-backend/src/test/java/com/kidcompass/KidcompassBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kidcompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidcompassBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
