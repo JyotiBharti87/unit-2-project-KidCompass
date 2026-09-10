b0b54f17ed7a1d63ff8826784dd272856d886b31
jyotibharti87 <joybharti87@gmail.com>
2026-09-09 20:19:04 -0500

    commit-21

--- kidcompass-backend/src/main/java/com/kidcompass/controllers/EventController.java
+++ kidcompass-backend/src/main/java/com/kidcompass/controllers/EventController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
+@CrossOrigin(origins = "http://localhost:5174")
 public class EventController {
 
     private final EventRepository eventRepository;
